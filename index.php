72bad799d85d4d9457ae107342498e46f5d43384
hackhisass <uditagarwal1997@gmail.com>
2016-03-06 18:44:41 +0530

    Added files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+
+
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head><link rel="icon" 
+      type="image/png" 
+      href="images/logo.jpg">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/ImageTrans.css" />
+		<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>Collegespace</title>
+</head>
+<body>
+	<div class="cd-header navbar-fixed-top" >
+		<a href="#0" class="cd-logo"><img src="./images/logo.jpg" style="height:70px; width:70px;" alt="Logo"></a>
+		<a href="#0" class="cd-3d-nav-trigger">
+			Menu
+			<span></span>
+		</a>
+	</div>
+ <!-- .cd-header -->
+	
+	
+
+
+    <div class="jumb">
+
+        <ul class="cb-slideshow">
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+        </ul>
+    </div>
+    
+    <!--
+    <div>
+    	asfasfafasfasf
+    </div>
+    <img src="./images/logo.jpg" style="height:70px; width:70px;" alt="Logo">
+    <img src="./images/logo.jpg" style="height:70px; width:70px;" alt="Logo">
+    <img src="./images/logo.jpg" style="height:70px; width:70px;" alt="Logo">-->
+</div>
+		<!-- all your content here -->
+		
+	<nav class="cd-3d-nav-container">
+		<ul class="cd-3d-nav">
+			<li class="cd-selected">
+				<a href="#top" class="smoothScroll">Home</a>
+			</li>
+
+			<li>
+				<a href="#studentscorner" class="smoothScroll">Students Corner</a>
+			</li>
+
+			<li>
+				<a href="#faculty" class="smoothScroll">Faculty</a>
+			</li>
+
+			<li>
+				<a href="#notices" class="smoothScroll">Notices</a>
+			</li>
+
+			<li>
+				<a href="#CSteam" class="smoothScroll">CS Team</a>
+			</li>
+		</ul> <!-- .cd-3d-nav -->
+
+		<span class="cd-marker color-1"></span>	
+	</nav> <!-- .cd-3d-nav-container -->
+	<?php
+	include('feed/main.php');
+	?>
+
+</body>
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/smoothscroll.js"></script>
+
+</html>
